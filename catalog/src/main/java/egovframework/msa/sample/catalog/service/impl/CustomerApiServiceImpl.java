e13c6dd0c788b2e2e111f83da02210c3fea972e1
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-06-15 13:22:02 +0900

    feat: Customer서비스와 Catalog서비스 연결
    Catalog서비스에서 Customer서비스를 호출하도록 기능 수정

--- catalog/src/main/java/egovframework/msa/sample/catalog/service/impl/CustomerApiServiceImpl.java
+++ catalog/src/main/java/egovframework/msa/sample/catalog/service/impl/CustomerApiServiceImpl.java
@@ -1,14 +1,19 @@
 package egovframework.msa.sample.catalog.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import egovframework.msa.sample.catalog.service.CustomerApiService;
 
 @Service
 public class CustomerApiServiceImpl implements CustomerApiService{
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public String getCustomerDetail(String customerId) {
-        return customerId;
+        return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
     }
 }
