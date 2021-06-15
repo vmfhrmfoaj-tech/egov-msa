61b302db17985ea69aeba093bf6a70c54bd6c061
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-06-15 16:38:38 +0900

    feat: MSA Circuit Breaker - Spring Cloud(Hystrix)
    Use Hystrix module for cloud circuit breaker
    Update Spring version 2.5.1 -> 2.2.6.RELEASE
    Add Fallback function when call customer apiwq

--- catalog/src/main/java/egovframework/msa/sample/catalog/CatalogApplication.java
+++ catalog/src/main/java/egovframework/msa/sample/catalog/CatalogApplication.java
@@ -2,11 +2,13 @@ package egovframework.msa.sample.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @ComponentScan("egovframework.*")
+@EnableCircuitBreaker
 @SpringBootApplication
 public class CatalogApplication {
 
