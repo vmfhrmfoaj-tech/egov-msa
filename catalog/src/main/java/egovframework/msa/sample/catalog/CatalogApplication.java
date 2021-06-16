d33fd0a3db2cbf599f026d4c8388698dd823604d
vmfhrmfoaj-tech <56380468+vmfhrmfoaj-tech@users.noreply.github.com>
2021-06-16 10:44:18 +0900

    PR feat -> develop (squash) (#13)
    * fix: Fix typo
    * feat: Add dependency - spring-boot-devtools
    * feat: Add maven dependency spring-cloud-starter-netflix-ribbon
    * feat: Add LoadBalanced annotation to RestTemplate Bean Object"
    * feat: Update customer service url to ribbon serviceId (customer)
    * feat: Add customer.ribbon.listOfServers property for Ribbon can find the serviceId
    
    close #12

--- catalog/src/main/java/egovframework/msa/sample/catalog/CatalogApplication.java
+++ catalog/src/main/java/egovframework/msa/sample/catalog/CatalogApplication.java
@@ -3,6 +3,7 @@ package egovframework.msa.sample.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class CatalogApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
